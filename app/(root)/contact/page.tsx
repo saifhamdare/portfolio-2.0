b5da36057d58f3acd106eaea674a32ae3090349b
Saif <hsaif1611@gmail.com>
2024-06-11 15:47:16 +0530

    updated the portfolio

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,50 @@
+import Socials from '@/components/Socials';
+import { LampContainer } from '@/components/UI/Lamp';
+import { about } from '@/utils/data';
+import { Link } from '@nextui-org/react';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+    title: "SS Portfolio | Contact",
+    openGraph: {
+      title: "SS Portfolio | Contact",
+      url: "https://saifhamdare-portfolio.netlify.app/contact",
+      images: [
+        {
+          url: "https://i.ibb.co/5k6rdvF/tabicon.png",
+          alt: "portfolio-logo",
+        },
+      ],
+    },
+    twitter: {
+      title: "SS Portfolio | Contact",
+      images: [
+        {
+          url: "https://i.ibb.co/5k6rdvF/tabicon.png",
+          alt: "portfolio-logo",
+        },
+      ],
+    },
+  };
+
+const Page = () => {
+    return (
+        <>
+            <LampContainer>
+                <div className='container text-center'>
+                    <h2 className='text-sm md:text-lg mb-4'>Contact me at</h2>
+                    <Link
+                        href={`mailto:${about.details.email}`}
+                        className='text-2xl md:text-4xl lg:text-7xl font-semibold text-gray-200 underline lg:mb-6'
+                    >
+                        {about.details.email}
+                    </Link>
+                    <Socials emailShow={false} />
+                </div>
+            </LampContainer>
+        </>
+    );
+};
+
+export default Page;
---
+++ utils/data.ts
@@ -0,0 +1,159 @@
+export const about = {
+    name: 'Saif Hamdare',
+    position: 'Frontend Engineer',
+    bio: `Full Stack Developer & Life Enthusiast: I’m the guy who’s been diving into code for over 3 years, playing around with cool stuff like MERN and MEAN. Whether it’s a startup vibe or the big league MNCs, I’ve been there, leading teams and making clients happy with solid tech solutions. When I’m not coding, you’ll catch me pumping iron, cruising on my bike, or chasing that coffee high. Just a regular day for me, living it up one line of code at a time!`,
+    socials: {
+        linkedIn: 'https://in.linkedin.com/in/saif-hamdare-245304136',
+        mail: 'saifhamdare@gmail.com',
+        github: 'https://github.com/saad696',
+    },
+    details: {
+        firstname: 'Saif',
+        lastname: 'Hamdare',
+        email: 'saifhamdare@gmail.com',
+        nationality: 'Indian',
+        degree: "Bachelor's",
+        address: 'Mumbai, Maharashtra',
+        interests: 'Motorcycling, Learning about new things',
+        experience: '2 Years',
+    },
+    skills: {
+        frontend: [
+            'HTML',
+            'CSS',
+            'Scss',
+            'Bootstrap',
+            'Tailwind',
+            'Javascript',
+            'ES6',
+            'Typescript',
+            'jQuery',
+            'React Js',
+            'Next Js',
+            'Redux',
+            'Material UI',
+            'Ant Design',
+            'Framer Motion',
+            "Shadcn"
+        ],
+        backend: [
+            'PHP (basic)',
+            'Node Js (basic)',
+            'Firebase'
+                ],
+        tools: ['GIT', 'Github', 'Postman', 'Jira'],
+        // soft: [
+        //     'Time management',
+        //     'Client/Stakeholders Interactions',
+        //     'Communication',
+        // ],
+    },
+};
+
+export const education = [
+    {
+        title: 'Bsc. Information Technology',
+        location: 'Mumbai University',
+        date: '2019 - 2021',
+        description: '',
+    },
+    {
+        title: 'Diploma - Computer Engineering',
+        location: 'Maharashtra State Board of Technical Education ',
+        date: '2015 - 2019',
+        description: '',
+    },
+];
+
+export const work = [
+    {
+        title: 'Software Engineer 1- CarePal Secure',
+        location: 'Mumbai, Maharashtra ',
+        date: 'Dec 2022 - Present',
+        description: `*   **Strategic Project Completion**: **I was specifically recalled to finalize the Visvesvaraya PhD Scheme project**, a formidable undertaking that demanded both resilience and ingenuity. My successful completion of this endeavor positioned me for an **extraordinary opportunity: contributing to a project aligned with the World Health Organization (WHO) - NVBD**. The chance to impact global health outcomes thrilled me, and I eagerly embraced my role in this pivotal initiative.`,
+    },
+    {
+        title: 'Frontend Developer (Intern) - Impactguru Crowdfunding Platform',
+        location: 'Mumbai, Maharashtra',
+        date: 'Sept 2022 - Dec 2022',
+        description: `
+*   **Led as Software Engineer 2 at a dynamic crowdfunding platform**, driving improvements in KYC, fundraiser listing, and performance pages, as well as NGO, personal cause, and creative idea fundraiser creation whilst following agile methodology daily scrums and 15 days sprints.
+*   Strategically revamped the KYC module, **increasing document upload rates from 6% to an impressive 65%**.
+*   Enhanced load time and efficiency of fundraiser listing and performance page, resulting in an 8% improvement, while **reducing fundraiser creation failure rates by 10%**, significantly boosting overall efficiency.
+        `,
+    },
+    {
+        title: 'Nestable Life',
+        location: 'Mumbai, Maharashtra',
+        date: 'Mar 2022 - Jun 2022',
+        description: `*   As a Code Instructor, I teach front-end technologies like HTML5, CSS3, Bootstrap, JavaScript, React.js, Antd, GitHub, and TypeScript. My goal is to prepare students for the job market by making them proficient in these technologies.`,
+    },
+    
+    
+];
+
+export const projects = [
+    {
+        projectName: 'CarePal Secure',
+        projectDesc:
+            ' Need to add Developed an static restaurant website with four pages which are Home, About us, Menu, Contact us. Integrated Google Maps location, YouTube Video, Instagram Feed, Social Media Re-directs.',
+        projectThumbnail: '/projects-ss/pl1.png',
+        duration: '6 Months',
+        techstack: 'ReactJS, React-Bootstrap, antDesign',
+        workDone: 'Frontend',
+        hostedUrl: 'https://carePalsecure.com',
+        association: 'CarePal Secure Pvt. Ltd.',
+        images: [
+            '/projects-ss/nd1.png',
+            '/projects-ss/nd2.png',
+            '/projects-ss/nd3.png',
+            '/projects-ss/nd4.png',
+            '/projects-ss/nd5.png',
+        ],
+    },
+
+    {
+        projectName: 'Impactguru Crowdfunding Platform',
+        projectDesc:
+            'Need to add Impactguru is an online fundraising platform that empowers people, like you, to give to non-profits, individual causes, social enterprises, and start-ups. Our crowdfunding platform allows you to easily raise money online from individuals and organizations that share your passion, each making small donations that add up to a lot, in turn addressing critical social challenges in India.',
+        projectThumbnail: '/projects-ss/pl6.png',
+        duration: '18 Months',
+        techstack: 'React, Bootstrap, Scss, HTML, JavaScript, JQuery, Git, Github, PHP, MySQL',
+        workDone: 'Frontend',
+        hostedUrl: 'http://impactguru.com',
+        association: 'Impactguru Crowdfunding Platform',
+        images: [
+            '/projects-ss/ig-1.png',
+            '/projects-ss/ig-2.png',
+            '/projects-ss/ig-3.png',
+            '/projects-ss/ig-4.png',
+            '/projects-ss/ig-5.png',
+        ],
+    },
+
+    {
+        projectName: 'VikinX',
+        projectDesc: `My very own startup for motorcycle enthusiasts. Consisting of a client and an admin portal. 
+            developed admin and client from scratch primarily using Next v14 and React v18.
+            VikinX Mission: Our mission is to empower riders with technology that enhances their riding
+            experience, fosters a sense of community, and promotes safety. We aim to provide a platform where
+            riders can connect, share experiences, and manage their rides effortlessly, making every journey a
+            memorable one. Connecting the riders across.`,
+        projectThumbnail: '/projects-ss/pl8.png',
+        duration: '1 Months',
+        techstack: 'Next.js, Typescript, Clerk, NextUI, Firebase, Node.js, Express.js, React.js, Ant Design, Cloudinary, Vercel, Digital Ocean, Tailwind',
+        workDone: 'Frontend',
+        hostedUrl: 'https://vikinx.in',
+        association: 'Personal',
+        images: [
+            '/projects-ss/vx1.png',
+            '/projects-ss/vx2.png',
+            '/projects-ss/vx3.png',
+            '/projects-ss/vx4.png',
+            '/projects-ss/vx5.png',
+            '/projects-ss/vx6.png',
+            '/projects-ss/vx7.png',
+            '/projects-ss/vx8.png',
+        ],
+    },
+];
